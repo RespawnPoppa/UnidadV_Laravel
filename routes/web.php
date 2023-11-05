d364e4aaed2163110eaa4d2d930c37f7c24b06cf
Jose Manuel <elementos94260@gmail.com>
2023-11-04 19:45:19 -0700

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pollo', function (){
+
+    return '<h3>Hola crayola</h3>';
+});
+
+Route::get('/busca-usuario/{name}/{lastname?}', function($name, $lastname = "Doe") {
+    return "User: " . $name . " " . $lastname;
+})->where(['name' => '[A-Za-z]+', 'lastname' => '[A-Za-z]+'])->name('filtrado');
+
+
+Route::get('/suma/{num1}/{num2}', function($num1, $num2){
+    return "La suma es de: " . ($num1 + $num2);
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+'])->name('suma');
+
+Route::get('/vista/{name}', function ($name) {
+    return view('prueba', ['name' => $name]);
+});
+
+Route::get('/prueba-controller/{name}',[UserController::class,'index']);

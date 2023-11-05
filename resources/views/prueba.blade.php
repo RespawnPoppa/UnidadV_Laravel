adac72a72cb9d544898f7bc59b20d855fc5eb0ef
Jose Manuel <elementos94260@gmail.com>
2023-11-05 10:45:50 -0700

    Agregación de rutas nuevas

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h3>Hola <?= $name ?></h3>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,22 @@ Route::get('/suma/{num1}/{num2}', function($num1, $num2){
     return "La suma es de: " . ($num1 + $num2);
 })->where(['num1' => '[0-9]+', 'num2' => '[0-9]+'])->name('suma');
 
+Route::get('/multiplicacion/{num1}/{num2}', function($num1, $num2){
+    return "El resultado de la multiplicación es: " . ($num1 * $num2);
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+'])->name('multiplicacion');
+
+Route::get('/division/{num1}/{num2}', function($num1, $num2){
+    if ($num2 == 0) {
+        return "No se puede dividir por cero.";
+    }
+
+    return "El resultado de la división es: " . ($num1 / $num2);
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+'])->name('division');
+
+Route::get('/resta/{num1}/{num2}', function($num1, $num2){
+    return "El resultado de la resta es: " . ($num1 - $num2);
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+'])->name('resta');
+
 Route::get('/vista/{name}', function ($name) {
     return view('prueba', ['name' => $name]);
 });
